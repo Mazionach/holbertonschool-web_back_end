8d8fb09ddce9e87169d9b1e723b2e3a0aff07436
Mazionach <mazafesio24@gmail.com>
2024-04-26 14:21:55 -0700

    Hotfix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,7 +1,9 @@
 export default function taskBlock(trueOrFalse) {
+  var a = trueOrFalse;
+  var b = a;
   const task = false;
   const task2 = true;
-  const dummy = trueOrFalse;
+  a = b;
 
   return [task, task2];
 }
