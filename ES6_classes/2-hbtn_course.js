3343f3430807a2bb307d176a0332a15285cf1545
Mazionach <mazafesio24@gmail.com>
2024-04-28 14:15:06 -0700

    Added course class

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(n) {
+    if (typeof(n) != "string") {
+      throw new Error("TypeError: Name must be a string");
+    }
+    this._name = n;
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(l) {
+    if (typeof(l) != "number") {
+      throw new Error("TypeError: Length must be a number");
+    }
+    this._length = l;
+  }
+
+  get students() {
+    return this._students;
+  }
+  set students(s) {
+    if (typeof(s) != "array") {
+      throw new Error("TypeError: Students must be an array of strings");
+    }
+    for (let i of s) {
+      if (typeof(i) != "string") {
+        throw new Error("TypeError: Students must be an array of strings");
+      }
+    }
+    this._students = s;
+  }
+}
