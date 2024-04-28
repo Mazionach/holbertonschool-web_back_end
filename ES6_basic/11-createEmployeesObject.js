fc319671d1fa2b6ce86e45547ce975d8312a41c8
Mazionach <mazafesio24@gmail.com>
2024-04-28 07:32:07 -0700

    Hotfix

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const obj = {
-    $departmentName: [
-      $employees,
-    ],
+    $departmentName: employees,
   };
   return obj;
 }
