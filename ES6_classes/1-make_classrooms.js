37ed78c76cb17ac515d13c02de67f5f4f5f142dd
Mazionach <mazafesio24@gmail.com>
2024-04-28 13:27:10 -0700

    Hotfix

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,13 @@
 import ClassRoom from "0-classroom.js";
 
 export default function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
   const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
+    class1,
+    class2,
+    class3
   ];
   return rooms;
 }
