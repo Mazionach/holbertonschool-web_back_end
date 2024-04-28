08b6e8a7c07216bbb076084322c4201f65acd610
Mazionach <mazafesio24@gmail.com>
2024-04-28 07:29:50 -0700

    Fixed format

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  let obj = {
+  const obj = {
     $departmentName: [
-       $employees,
+      $employees,
     ],
   };
   return obj;
