2516b51f894b289322e16766c973ac92dced9feb
Mazionach <mazafesio24@gmail.com>
2024-04-28 19:43:33 -0700

    Hotfix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,13 @@
-export default class Building {
+export abstract class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    try {
-      this.evacuationWarningMessage();
-    } catch (error) {
-      throw error;
-    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw Error('Class extending Building must override evacuationWarningMessage');
+  abstract evacuationWarningMessage() {
+    void;
   }
 }
