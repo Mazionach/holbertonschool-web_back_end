70d21a47c25c2f760f18b29fc9377ed98b450666
Mazionach <mazafesio24@gmail.com>
2024-04-28 18:39:49 -0700

    Added currency class

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    if (typeof c !== 'number') {
+      throw new TypeError('Code must be a number');
+    }
+    this._code = c;
+  }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`
+  }
+}
