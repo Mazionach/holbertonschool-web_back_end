469b47c2b93d0e6a978143e77ceb4366e12dcb5d
Mazionach <mazafesio24@gmail.com>
2024-04-28 19:12:16 -0700

    Hotfix

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(v) {
+    if (typeof v !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = v;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(s) {
+    this._currency = s;
+  }
+
+  displayFullPrice() {
+    return `${this.ammount} ${this.currency.name} (${this.currency.code})`
+  }
+
+  convertPrice(amount, conversionRate) {
+    return this.ammount * conversionRate;
+  }
+}
