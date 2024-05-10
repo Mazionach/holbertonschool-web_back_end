8a325ccc5b6b764a1fec79c80f7f7666139e79f5
Mazionach <mazafesio25@gmail.com>
2024-05-10 13:37:30 -0700

    Added hbtn class

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,13 @@
+export default class HolbertonClass {
+  constructor(size, alocation) {
+    this._size = size;
+    this._location = alocation;
+  }
+
+  toString() {
+    return this._location;
+  }
+  valueOf() {
+    return this._size;
+  }
+}
