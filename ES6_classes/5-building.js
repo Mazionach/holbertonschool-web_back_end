ffde0cd2ad0ace9abbfbcc57be3dac8dc9a3c739
Mazionach <mazafesio25@gmail.com>
2024-05-10 06:20:07 -0700

    Hotfix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,8 +2,8 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor.name !== 'Building') {
-      if (typeof this.evacuationWarningMessage !== "function") {
-        throw Error("Class extending Building must override evacuationWarningMessage");
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw Error('Class extending Building must override evacuationWarningMessage');
       }
     }
   }
--- ES6_classes/main.js
+++ ES6_classes/main.js
@@ -1,6 +1,13 @@
-import Building from "./5-building.js";
+import Building from './5-building.js';
 
-test("Building is implemented correctly", () => {
-  const building = new Building(200);
-  expect(building.sqft).toBe(200);
-});
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+    new TestBuilding(200)
+}
+catch(err) {
+    console.log(err);
+}
