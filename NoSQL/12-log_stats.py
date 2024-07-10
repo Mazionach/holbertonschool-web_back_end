3d42e831d31578ac62f5930da2ab41d8d95bceb5
Mazionach <mazafesio25@gmail.com>
2024-07-10 08:59:00 -0700

    Removed automatic execution

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,18 +7,25 @@ from pymongo import MongoClient
 
 
 
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def log_stats():
+    """
+    Log stats about database
+    """
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-cl = MongoClient("localhost", 27017)
-db = cl.logs
-col = db.ngix
+    cl = MongoClient("localhost", 27017)
+    db = cl.logs
+    col = db.ngix
 
-print(f"{col.count_documents({})} logs")
-print("Methods:")
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{col.count_documents({})} logs")
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-for m in method:
-    print(f"\tmethod {m}: {col.count_documents({ 'method': m })}")
-print(f"{col.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    for m in method:
+        print(f"\tmethod {m}: {col.count_documents({ 'method': m })}")
+    print(f"{col.count_documents({'method': 'GET', 'path': '/status'})} status check")
 
+
+if __name__ == "__main__":
+    log_stats()
