c68fce5f79c3be2434b61d77d9063e1d786ddf22
Mazionach <mazafesio25@gmail.com>
2024-07-11 23:09:49 +0000

    Typo, added resolve all promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`First Name: ${user.firstName}, Last Name: ${user.lastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignUp;
