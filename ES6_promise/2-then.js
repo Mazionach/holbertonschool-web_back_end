5f524d8251d48ca9caaf66da43d5c1b2f8d3b2e6
Mazionach <mazafesio25@gmail.com>
2024-07-11 23:04:39 +0000

    Fixed style

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    ({ status: 200, body: 'success' })
-  }).catch(() => {
-    (new Error())
-  }).finally(() => {
+  return promise.then(() => ({ status: 200, body: 'success' }))
+  .catch(() => (new Error()))
+  .finally(() => {
     console.log('Got a response from the API');
   });
 }
